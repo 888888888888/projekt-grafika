d8829a1d5afa6851339f057d97131941b458b5e8
artir2d2 <pusniaczek@gmail.com>
2015-01-03 19:34:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-// include the basic windows header files and the Direct3D header files
 #include <windows.h>  //biblioteka do okienka
 #include <windowsx.h> //tu tez
 #include <string>
@@ -7,7 +5,6 @@
     #include <cstdlib>
 #else
     #include <stdlib.h>
-
 #endif
 #include <fstream>
 #include <SDL.h>
@@ -15,93 +12,71 @@
 #include <iostream>
 #include <vector>
 
+/*DEFINICJE STALYCH I ID*/
 #define SCREEN_WIDTH  800	//definiujemy szerokosc i wysokosc ekranu w trybie okna
 #define SCREEN_HEIGHT 600
-#define ID_Color 111	//id przycisków
-#define ID_Convert 222	//id Przycisku konwertuj
+#define ID_Color 111	//id przyciskÃ³w
+#define ID_Convert 555	//id Przycisku konwertuj
+/*************************/
+
 using namespace std;
+
 SDL_Surface *screen;
-int width = 900;
+int width = 900;  //wielkosc okna graficznego
 int height = 600;
 char const* tytul = "Okno obrazka";
+
+int w=0, h=0; //wielkosc pliku graficznego
+
 LPSTR Bufor;
 DWORD dlugosc;
- int w=0, h=0; //wielkosc pliku
 
+HWND hWnd,view;			//uchwyty okna
+HWND hCombo;
+HWND hText;
 fstream plik1;
 int frame();
+void Color(int TrybKoloru);
 void newfile();
 void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B);
 void byterun(SDL_Color a[], int length);
 void czyscEkran(Uint8 R, Uint8 G, Uint8 B);
 void ladujBMP(char const* nazwa, int x, int y);
 SDL_Color getPixel (int x, int y);
-//void byterun(signed char a[], int length)
-void byterun(SDL_Color a[], int length)
+
+void byterun(SDL_Color dane[], int length)
 {
 int i = 0;
-
-//dopoki wszystkie bajty nie sa skompresowane
-while (i < length)
-{
-//sekwencja powtarzajacych sie bajtow
-if ((i < length-1) &&
-(a[i].r == a[i+1].r)&&
-(a[i].g == a[i+1].g)&&
-(a[i].b == a[i+1].b))
-{
-//zmierz dlugosc sekwencji
-int j = 0;
-while ((i+j < length-1) &&
-(a[i+j].r == a[i+j+1].r) &&
-    (a[i+j].g == a[i+j+1].g) &&
-    (a[i+j].b == a[i+j+1].b) &&
-(j < 127))
-{
-j++;
-}
-//wypisz spakowana sekwencje
-plik1 << -j << a[i+j].r <<a[i+j].g <<a[i+j].b ;
-//przesun wskaznik o dlugosc sekwencji
-i += (j+1);
-}
-//sekwencja roznych bajtow
-else
-{
-//zmierz dlugosc sekwencji
-int j=0;
-while ((i+j < length-1) &&
-((a[i+j].r != a[j+i+1].r) || (a[i+j].g != a[j+i+1].g) || (a[i+j].b != a[j+i+1].b)) &&
-(j <128))
-{
-j++;
-}
-//dodaj jeszcze koncowke
-if ((i+j == length-1) &&
-(j < 128))
-{
-j++;
-}
-//wypisz spakowana sekwencje
-plik1 << (j-1);
-for (int k=0; k<j; k++)
-{
-plik1 << a[i+k].r << a[i+k].g  <<a[i+k].b ;
-}
-//przesun wskaznik o dlugosc sekwencji
-i += j;
-}
-}
+    while (i < length)
+    {
+        if ((i < length-1) && (dane[i].r == dane[i+1].r) && (dane[i].g == dane[i+1].g) && (dane[i].b == dane[i+1].b))
+        {
+            int j = 0;
+            while ((i+j < length-1) && (dane[i+j].r == dane[i+j+1].r) && (dane[i+j].g == dane[i+j+1].g) && (dane[i+j].b == dane[i+j+1].b) && (j < 127))  j++;
+            plik1 << -j << dane[i+j].r <<dane[i+j].g <<dane[i+j].b ; //zapis skompresowanych danych do pliku
+            i += (j+1);
+        }
+        else
+        {
+            int j=0;
+            while ((i+j < length-1) && ((dane[i+j].r != dane[j+i+1].r) || (dane[i+j].g != dane[j+i+1].g) || (dane[i+j].b != dane[j+i+1].b)) && (j <128)) j++;
+            //dodaj jeszcze koncowke
+            if ((i+j == length-1) && (j < 128)) j++;
+            plik1 << (j-1); //zapis do pliku danych ktÃ³rych nie da siÄ™ skompresowaÄ‡
+            for (int k=0; k<j; k++)  plik1 << dane[i+k].r << dane[i+k].g  <<dane[i+k].b ;
+            i += j;
+        }
+    }
 }
 
 void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B)
 {
   if ((x>=0) && (x<width) && (y>=0) && (y<height))
   {
-    /* Zamieniamy poszczególne skladowe koloru na format koloru pixela */
+    /* Zamieniamy poszczegÃ³lne skladowe koloru na format koloru pixela */
     Uint32 pixel = SDL_MapRGB(screen->format, R, G, B);
 
-    /* Pobieramy informacji ile bajtów zajmuje jeden pixel */
+    /* Pobieramy informacji ile bajtÃ³w zajmuje jeden pixel */
     int bpp = screen->format->BytesPerPixel;
 
     /* Obliczamy adres pixela */
@@ -142,32 +117,20 @@ void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B)
 void newfile(int w, int h)
 {
 /*****************************TWORZENIE PLIKU**************************************/
-    //plik1 = fopen( "converted.art", "w+" );
-
     plik1.open( "converted.art", std::ios::in | std::ios::out );
-    if( plik1.good() == true )
-{
-    cout << "Uzyskano dostep do pliku!" << endl;
-    //tu operacje na pliku
-} else cout << "Dostep do pliku zostal zabroniony!" << endl;
+    if( plik1.good() == true ) cout << "Uzyskano dostep do pliku!" << endl;
+    else cout << "Dostep do pliku zostal zabroniony!" << endl;
 /***********************************TWORZYENIE TABLICY PRZECHOWUJACEJ SUROWE DANE********************************/
-   // SDL_Color **pliknowy=new SDL_Color * [w];
-   // for(int i=0;i<w;i++) pliknowy[i]=new SDL_Color[h];
     SDL_Color *pliknowy;
     SDL_Color  bufor;
     pliknowy=new SDL_Color  [w*h];
     int p=0;
     for(int x=0;x<w;x++)
-    for(int y=0;y<h;y++)
-    {
-    bufor=getPixel(x,y);
-
-    pliknowy[p++]=bufor;
-   // plik1<<bufor.r;
-   // plik1<<bufor.g;
-   // plik1<<bufor.b;
-    }
-    //for(int i=0;i<20;i++) cout<<pliknowy[i];
+        for(int y=0;y<h;y++)
+        {
+            bufor=getPixel(x,y);
+            pliknowy[p++]=bufor;
+        }
 /*******************************KOMPRESJA DANYCH Z TABLICY ********************************/
 byterun(pliknowy,w*h);
 plik1.close();
@@ -187,23 +150,18 @@ void ladujBMP(char const* nazwa, int x, int y)
         dstrect.x = x;
         dstrect.y = y;
         SDL_BlitSurface(bmp, 0, screen, &dstrect);
-        w=bmp->w;
+        w=bmp->w;  //ustalamy wielkosc wczytanego obrazka
         h=bmp->h;
         SDL_Flip(screen);
         SDL_FreeSurface(bmp);
     }
-
-
 }
-
 
 void czyscEkran(Uint8 R, Uint8 G, Uint8 B)
 {
     SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, R, G, B));
     SDL_Flip(screen);
-
 }
-
 
 SDL_Color getPixel (int x, int y) {
     SDL_Color color ;
@@ -244,8 +202,6 @@ int frame()
     }
 
     SDL_WM_SetCaption( tytul , NULL );
-
-
     ladujBMP(Bufor, 0, 0);
 }
 
@@ -254,16 +210,16 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 
 HWND g_hPrzycisk;
 // the entry point for any Windows program  //tu mamy funkcje ktora jest odpowiedznikiem funkcji main,
-int WINAPI WinMain(HINSTANCE hInstance,		//1. HINSTANCE hInstance - uchwyt, który jest liczba za pomoca ktorej winows identyfikuje aplikacje i okna
+int WINAPI WinMain(HINSTANCE hInstance,		//1. HINSTANCE hInstance - uchwyt, ktÃ³ry jest liczba za pomoca ktorej winows identyfikuje aplikacje i okna
 	HINSTANCE hPrevInstance,					//2. HINSTANCE hPrevInstance - to jakis przestarzaly parametr informujacy o poprzedniej kopii
 	LPSTR lpCmdLine,							//3. LPSTR lpCmdLine - ten parametr sprawia ze gdy dopiszemy w cmd przy run "nazwaaplikacji.exe" np. "runA" to bedzie mozna odpalic kilka okien o specjalnych parametrach (nie bardzo wiem jakich)
 	int nCmdShow)  								//4. int nCmdShow - ten parametr odpowiada za to jak okno ma sie pojawic (zminimalizowane/zmaksymalizowane/w oknie	SW_SHOWNORMAL	SW_SHOW	SW_HIDE	SW_MAXIMIZE		SW_MINIMIZE	SW_RESTORE	SW_SHOWMAXIMIZED	SW_SHOWMINIMIZED	SW_SHOWMINNOACTIVE	SW_SHOWNA  SW_SHOWNOACTIVATE
 {
-	HWND hWnd,view;			//uchwyt okna, uzupe³niany przez funkcjê
+
 	WNDCLASSEX wc;		//struktura majaca informacje o klasie okna
 
 	ZeroMemory(&wc, sizeof(WNDCLASSEX)); //zerowanie prestrzeni pamieci dla struktury klasy okna (wc- window class)
-	//wype³nianie strukturki
+	//wypeÂ³nianie strukturki
 	wc.cbSize = sizeof(WNDCLASSEX);
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.lpfnWndProc = WindowProc;
@@ -289,17 +245,11 @@ int WINAPI WinMain(HINSTANCE hInstance,		//1. HINSTANCE hInstance - uchwyt, któr
 		hInstance,
 		NULL);
 
-	//pokaz okno
-	ShowWindow(hWnd, nCmdShow);
-
-
-	// enter the main loop:
+	ShowWindow(hWnd, nCmdShow); 	//pokaz okno
 
 	MSG msg;  //ta struktura przechowuje info o wydarzeniach z nia zwiazanych
-
-	//*************************************
-
-	HWND hCombo = CreateWindowEx(WS_EX_CLIENTEDGE, "COMBOBOX", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER |
+	//*************************************TU WSZYSTKIE KONTROLKI
+    hCombo = CreateWindowEx(WS_EX_CLIENTEDGE, "COMBOBOX", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER |
 		CBS_DROPDOWN, 5, 50, 150, 200, hWnd, (HMENU)ID_Color, hInstance, NULL);
 
 	SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM) "RGB");
@@ -307,27 +257,21 @@ int WINAPI WinMain(HINSTANCE hInstance,		//1. HINSTANCE hInstance - uchwyt, któr
 	SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM) "HSL");
 	SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM) "YUV");
 
-	//*************************************
 
-	HWND hText = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER |   //sciezka dostepu do pliku
-		WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL, 5, 5, 475, 40, hWnd, NULL, hInstance, NULL);
-	SetWindowText(hText, "C:\\Users\\siusiak\\Desktop\\matki na studia\\rok2\\grafika\\projekt\\main\\projekt\\obrazek1.bmp");
+    //sciezka dostepu do pliku
+    hText = CreateWindowEx(WS_EX_CLIENTEDGE, "EDIT", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER |
+    WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL, 5, 5, 475, 40, hWnd, NULL, hInstance, NULL);
+	SetWindowText(hText, "Wpisz tu sciezke do pliku");
 
+       // dlugosc = GetWindowTextLength(hText);
+        //Bufor = (LPSTR)GlobalAlloc(GPTR, dlugosc + 1);
+       // GetWindowText(hText, Bufor, dlugosc + 1); // pobralem sciezke dostepu do zmiennej bufor
 
-
-            dlugosc = GetWindowTextLength(hText);
-            Bufor = (LPSTR)GlobalAlloc(GPTR, dlugosc + 1);
-            GetWindowText(hText, Bufor, dlugosc + 1); // pobralem sciezke dostepu do zmiennej bufor
-
-	//*************************************
 
 	g_hPrzycisk = CreateWindowEx(0, "BUTTON", "Konwertuj", WS_CHILD | WS_VISIBLE,
 		250, 60, 150, 30, hWnd, (HMENU)ID_Convert, hInstance, NULL);
 
-	//*************************************
-
-	//CreateFile(sciezka, GENERIC_READ, dwShareMode,
-	//	lpSecurityAttributes, dwCreationDistribution, dwFlagsAndAttributes, hTemplateFile)
+	//*************************************TU KONIEC KONTROLEK
 
 	while (TRUE)
 	{
@@ -337,7 +281,7 @@ int WINAPI WinMain(HINSTANCE hInstance,		//1. HINSTANCE hInstance - uchwyt, któr
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 			if (msg.message == WM_QUIT)
-				break;
+            break;
 		}
 
 
@@ -347,107 +291,40 @@ int WINAPI WinMain(HINSTANCE hInstance,		//1. HINSTANCE hInstance - uchwyt, któr
 	return msg.wParam;
 }
 /*************************************************************************************************************************************/
-// this is the main message handler for the program
+
+/*OBSLUGA KOMUNIKATOW*/
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	switch (msg)
 	{
-	case WM_CLOSE:
-		DestroyWindow(hWnd);
-		break;
-
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	case WM_COMMAND:
-		switch (wParam)
-		{
-		case ID_Color:
-			MessageBox(hWnd, "Wcisnoles przycisk 1", "Test", MB_ICONINFORMATION);
-			break;
-		case ID_Convert:
-
-			frame();
-			newfile(w,h);
-
-			break;
-		}
-
-	default:
-		return DefWindowProc(hWnd, msg, wParam, lParam);
+/**************************/
+        case WM_CLOSE:
+            DestroyWindow(hWnd);
+            break;
+/**************************/
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+/**************************/
+        case WM_COMMAND:
+            switch (wParam)
+            {
+            /**************************/
+            case ID_Convert:
+                int TrybKoloru = ComboBox_GetCurSel( hCombo );
+                dlugosc = GetWindowTextLength(hText);
+                Bufor = (LPSTR)GlobalAlloc(GPTR, dlugosc + 1);
+                GetWindowText(hText, Bufor, dlugosc + 1); // pobralem sciezke dostepu do zmiennej bufor
+                frame();
+                newfile(w,h);
+                break;
+            /**************************/
+            }
+         break;
+/**************************/
+        default:
+            return DefWindowProc(hWnd, msg, wParam, lParam);
 	}
 
 	return 0;
 }
-
-
-
-/*
-int main ( int argc, char** argv )
-{
-    // console output
-    freopen( "CON", "wt", stdout );
-    freopen( "CON", "wt", stderr );
-
-    // initialize SDL video
-    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "Unable to init SDL: %s\n", SDL_GetError() );
-        return 1;
-    }
-
-    // make sure SDL cleans up before exit
-    atexit(SDL_Quit);
-
-    // create a new window
-    screen = SDL_SetVideoMode(width, height, 32,
-                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
-    if ( !screen )
-    {
-        printf("Unable to set video: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_WM_SetCaption( tytul , NULL );
-    // program main loop
-    bool done = false;
-    while (!done)
-    {
-        // message processing loop
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            // check for messages
-            switch (event.type)
-            {
-                // exit if the window is closed
-            case SDL_QUIT:
-                done = true;
-                break;
-
-                // check for keypresses
-            case SDL_KEYDOWN:
-                {
-                    // exit if ESCAPE is pressed
-                    if (event.key.keysym.sym == SDLK_ESCAPE)
-                        done = true;
-                    if (event.key.keysym.sym == SDLK_a)
-                        ladujBMP("obrazek1.bmp", 0, 0);
-                    if (event.key.keysym.sym == SDLK_s)
-                        ladujBMP("obrazek4.bmp", 0, 0);
-
-                    if (event.key.keysym.sym == SDLK_b)
-                        czyscEkran(10, 0, 0);          break;
-                     }
-            } // end switch
-        } // end of message processing
-
-    } // end main loop
-
-
-    // all is well ;)
-    printf("Exited cleanly\n");
-    return 0;
-}
-*/
-
